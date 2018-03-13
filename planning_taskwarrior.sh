5d4854d285b3653bde6a835f7d9f030d8d546695
Lluis <lluis.matasserrato@unifr.ch>
2018-03-13 13:55:34 +0100

    Added some taks in the PhD project

--- planning_taskwarrior.sh
+++ planning_taskwarrior.sh
@@ -3,3 +3,6 @@
 task add project:Origins.tracking Label the entire tissue
 task add project:Origins.tracking Image the whole migration
 task add project:Origins.tracking Track all cells
+
+task add project:PhD.thesis Find a suitable topic
+taks add project:PhD.thesis Get enough credits
