a3976070dcf758930e2903b295a59a59fac78377
Simon Blanchoud <simon.blanchoud@gmail.com>
2018-03-13 13:58:15 +0100

    Trying to purge the tasks

--- planning_taskwarrior.sh
+++ planning_taskwarrior.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+task $(task uuids) delete
+
 task add project:Origins.tracking Label the entire tissue
 task add project:Origins.tracking Image the whole migration
 task add project:Origins.tracking Track all cells
